48d19ec9e50ab4b07d51a8cf34b167a7e672f2bf
Mira <kruzinka@yandex.ru>
2025-01-09 23:02:19 +0300

    Logic mostly done

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,47 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const todos = ref([])
+const newTodo = ref('')
+
+function handleSubmit() {
+  addNewTodo()
+}
+
+function addNewTodo() {
+  if (!newTodo.value) return
+
+  todos.value.push(newTodo.value)
+  newTodo.value = ''
+}
+
+function deleteTodo(index) {
+  todos.value = todos.value.filter((_, id) => id !== index)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!</h1>
+    <input type="text" v-model="newTodo" @keyup.enter="addNewTodo" placeholder="Enter a todo" />
+    <button @click="addNewTodo">Add</button>
+    <ul v-for="(todo, index) in todos" :key="index">
+      <li>
+        <input type="checkbox" />
+        <p>{{ todo }}</p>
+        <button @click="deleteTodo(index)">Del</button>
+      </li>
+    </ul>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
