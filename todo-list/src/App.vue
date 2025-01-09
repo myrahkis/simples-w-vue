dc2dc60a150eaeac5809750bc14b501999245fbc
Mira <kruzinka@yandex.ru>
2025-01-10 00:02:49 +0300

    i lied. now logic is almost done (check/delete checked)

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,38 +1,58 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const todos = ref([])
-const newTodo = ref('')
+const todo = ref(null)
 
-function handleSubmit() {
-  addNewTodo()
-}
+const finished = computed(() => todos.value.filter((todo) => todo.done).length);
+const listLength = computed(() => todos.value.length);
 
 function addNewTodo() {
-  if (!newTodo.value) return
+  if (!todo.value) return
 
-  todos.value.push(newTodo.value)
-  newTodo.value = ''
+  const newTodo = {
+    todo: todo.value,
+    done: false,
+  }
+
+  todos.value.push(newTodo)
+  todo.value = null
 }
 
-function deleteTodo(index) {
-  todos.value = todos.value.filter((_, id) => id !== index)
+function checkTodo(index) {
+  todos.value = todos.value.map((todo, id) => (id === index ? { ...todo, done: !todo.done } : todo))
+}
+
+function deleteChecked() {
+  todos.value = todos.value.filter((todo) => !todo.done)
+}
+
+function clearList() {
+  todos.value = []
 }
 </script>
 
 <template>
   <main>
     <h1>HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!</h1>
-    <input type="text" v-model="newTodo" @keyup.enter="addNewTodo" placeholder="Enter a todo" />
+    <input type="text" v-model="todo" @keyup.enter="addNewTodo" placeholder="Enter a todo" />
     <button @click="addNewTodo">Add</button>
     <ul v-for="(todo, index) in todos" :key="index">
-      <li>
-        <input type="checkbox" />
-        <p>{{ todo }}</p>
-        <button @click="deleteTodo(index)">Del</button>
+      <li class="row">
+        <input type="checkbox" :checked="todo.done" @click="checkTodo(index)" />
+        <p>{{ todo.todo }}</p>
       </li>
     </ul>
+    <button @click="deleteChecked">Delete finished</button>
+    <button @click="clearList">Clear list</button>
   </main>
+  <div v-if="listLength > 0">
+    <p>You completed {{ finished }} out of {{ listLength }}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.row {
+  display: flex;
+}
+</style>
