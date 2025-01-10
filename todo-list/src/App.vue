cabbad42a4846e739146f4bbbd2e5fdd1f90f60a
Mira <kruzinka@yandex.ru>
2025-01-10 16:52:18 +0300

    all logic done (added list soring)

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -3,9 +3,11 @@ import { computed, ref } from 'vue'
 
 const todos = ref([])
 const todo = ref(null)
+const order = ref('input')
 
-const finished = computed(() => todos.value.filter((todo) => todo.done).length);
-const listLength = computed(() => todos.value.length);
+const finished = computed(() => todos.value.filter((todo) => todo.done).length)
+const listLength = computed(() => todos.value.length)
+const percentage = computed(() => Math.floor((finished.value / listLength.value) * 100))
 
 function addNewTodo() {
   if (!todo.value) return
@@ -30,6 +32,18 @@ function deleteChecked() {
 function clearList() {
   todos.value = []
 }
+
+const sortingFunctions = {
+  completion: (a, b) => +a.done - +b.done,
+  alph: (a, b) => a.todo.localeCompare(b.todo),
+  'rev-alph': (a, b) => b.todo.localeCompare(a.todo),
+}
+
+const sortedTodos = computed(() => {
+  const todosCopy = [...todos.value]
+  const sortFunc = sortingFunctions[order.value]
+  return sortFunc ? todosCopy.sort(sortFunc) : todosCopy
+})
 </script>
 
 <template>
@@ -37,7 +51,7 @@ function clearList() {
     <h1>HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!</h1>
     <input type="text" v-model="todo" @keyup.enter="addNewTodo" placeholder="Enter a todo" />
     <button @click="addNewTodo">Add</button>
-    <ul v-for="(todo, index) in todos" :key="index">
+    <ul v-for="(todo, index) in sortedTodos" :key="index">
       <li class="row">
         <input type="checkbox" :checked="todo.done" @click="checkTodo(index)" />
         <p>{{ todo.todo }}</p>
@@ -47,7 +61,19 @@ function clearList() {
     <button @click="clearList">Clear list</button>
   </main>
   <div v-if="listLength > 0">
-    <p>You completed {{ finished }} out of {{ listLength }}</p>
+    <p v-if="finished === 0">Start completing your todos to see progress.</p>
+    <p v-else-if="finished === listLength">You've completed it all!</p>
+    <p v-else>You've completed {{ finished }} out of {{ listLength }}. That's {{ percentage }}%!</p>
+  </div>
+  <div>
+    <hr />
+    <p>Sort by:</p>
+    <select name="sort" v-model="order">
+      <option value="input">Input order</option>
+      <option value="completion">Completion</option>
+      <option value="alph">First letter (A-Z)</option>
+      <option value="rev-alph">First letter reversed (Z-A)</option>
+    </select>
   </div>
 </template>
 
